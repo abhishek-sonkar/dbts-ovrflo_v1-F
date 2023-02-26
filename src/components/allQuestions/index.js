f45995f146c7f4af5ecebe7e2efbea9744e73648
Abhishek Sonkar <51050033+abhishek-sonkar@users.noreply.github.com>
2023-02-26 20:50:36 +0530

    deploy-to-render

---
+++ src/components/allQuestions/index.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import history from '../../history';
+
+export default function Questions() {
+    const [quesArray, setQuesArray] = useState([]);
+    
+    useEffect(() => {
+        const temp = [];
+        fetch("https://dbts-ovrflo-v1-b.onrender.com/all-questions", {
+            headers: {
+                "x-access-token": localStorage.getItem("token"),
+            },
+        })
+        .then((res) => res.json())
+        .then((r) => {
+            r.forEach((element) => {
+                temp.push({
+                    title: element.question.title,
+                    id: element._id,
+                    askedBy: element.question.postedBy,
+                    date: element.question.postedOnDate,
+                    totalAnswers: element.answers.length,
+                });
+            });
+            setQuesArray([...temp]);
+        });
+      }, []);
+    
+    const tableData = (currQues, idx) => {
+        return (
+            <tr key={idx}>
+                <td onClick={() => history.push('/solve-question?id=' + currQues.id)}>
+                    <h4>{currQues.title}</h4><br/>
+                    <p><b>Asked By: </b>{currQues.askedBy} <b>On: </b>{currQues.date}</p>
+                    <p><b>Total answers: </b>{currQues.totalAnswers}</p>
+                </td>
+            </tr>);
+    };
+    return (
+        <div className="auth-inner" style={{ overflow: 'auto', width: '98%' }}>
+            <table className="table table-hover">
+                <tbody>{quesArray.map(tableData)}</tbody>
+            </table>
+        </div>
+    );
+}
